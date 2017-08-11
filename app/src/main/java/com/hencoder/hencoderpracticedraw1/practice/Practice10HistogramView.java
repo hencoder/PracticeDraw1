5c90d24de0db01ca825f5e48b7e7577b6fad3712
刘世麟 <503233512@qq.com>
2017-08-11 11:28:10 +0800

    丢失上传“

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -35,28 +35,28 @@ public class Practice10HistogramView extends View {
         float width = 70;
 
         mPaint.setColor(Color.WHITE);
-        mPaint.setStrokeWidth(PxDpUtil.dip2px(getContext(),1));
-        canvas.drawLine(50,50,50,600,mPaint); // 纵坐标
+        mPaint.setStrokeWidth(PxDpUtil.dip2px(getContext(), 1));
+        canvas.drawLine(50, 50, 50, 600, mPaint); // 纵坐标
 
-        canvas.drawLine(50,600,600,600,mPaint); // 横坐标
+        canvas.drawLine(50, 600, 600, 600, mPaint); // 横坐标
 
         mPaint.setTextSize(18);
-        canvas.drawText("Froyo",85,650,mPaint);
-        canvas.drawText("GB",85+width*1,650,mPaint);
-        canvas.drawText("ICS",85+width*2,650,mPaint);
-        canvas.drawText("JB",85+width*3,650,mPaint);
-        canvas.drawText("KitKat",85+width*4,650,mPaint);
-        canvas.drawText("L",85+width*5,650,mPaint);
-        canvas.drawText("M",85+width*6,650,mPaint);
+        canvas.drawText("Froyo", 85, 650, mPaint);
+        canvas.drawText("GB", 85 + width * 1, 650, mPaint);
+        canvas.drawText("ICS", 85 + width * 2, 650, mPaint);
+        canvas.drawText("JB", 85 + width * 3, 650, mPaint);
+        canvas.drawText("KitKat", 85 + width * 4, 650, mPaint);
+        canvas.drawText("L", 85 + width * 5, 650, mPaint);
+        canvas.drawText("M", 85 + width * 6, 650, mPaint);
 
         mPaint.setColor(Color.GREEN);
-        mPaint.setStrokeWidth(PxDpUtil.dip2px(getContext(),20));
-        canvas.drawLine(110,600,110,580,mPaint);
-        canvas.drawLine(110+width,600,110+width,550,mPaint);
-        canvas.drawLine(110+width*2,600,110+width*2,550,mPaint);
-        canvas.drawLine(110+width*3,600,110+width*3,450,mPaint);
-        canvas.drawLine(110+width*4,600,110+width*4,300,mPaint);
-        canvas.drawLine(110+width*5,600,110+width*5,250,mPaint);
-        canvas.drawLine(110+width*6,600,110+width*6,480,mPaint);
+        mPaint.setStrokeWidth(PxDpUtil.dip2px(getContext(), 20));
+        canvas.drawLine(110, 600, 110, 580, mPaint);
+        canvas.drawLine(110 + width, 600, 110 + width, 550, mPaint);
+        canvas.drawLine(110 + width * 2, 600, 110 + width * 2, 550, mPaint);
+        canvas.drawLine(110 + width * 3, 600, 110 + width * 3, 450, mPaint);
+        canvas.drawLine(110 + width * 4, 600, 110 + width * 4, 300, mPaint);
+        canvas.drawLine(110 + width * 5, 600, 110 + width * 5, 250, mPaint);
+        canvas.drawLine(110 + width * 6, 600, 110 + width * 6, 480, mPaint);
     }
 }
